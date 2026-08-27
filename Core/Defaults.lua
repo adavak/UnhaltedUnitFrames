2d32cb21dc980d2d9f09ec0ea71c978a9953662d
Adavak <adavak@adavak.com>
2026-08-27 10:31:43 +0800

    Default aura container size 32 to 20

--- Core/Defaults.lua
+++ Core/Defaults.lua
@@ -286,7 +286,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -511,7 +511,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -665,7 +665,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -862,7 +862,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1016,7 +1016,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1209,7 +1209,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1399,7 +1399,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1591,7 +1591,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1783,7 +1783,7 @@ local Defaults = {
                     },
                     Container = {
                         AnchorParent = "Frame",
-                        Size = 32,
+                        Size = 20,
                         Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
                         Num = 6,
                         Wrap = 6,
@@ -1973,7 +1973,7 @@ local Defaults = {
 						},
 						Container = {
 							AnchorParent = "Frame",
-							Size = 32,
+							Size = 20,
 							Layout = {"BOTTOMLEFT", "TOPLEFT", 0, 1, 1},
 							Num = 6,
 							Wrap = 6,
